6126fdeedf70f7d6ea7598e0b615f74594b39b88
alpertunga-bile <kingdom19online@hotmail.com>
2024-09-18 17:11:43 +0300

    remove value and head linked list funcs are added

--- includes/linked_list.h
+++ includes/linked_list.h
@@ -6,53 +6,83 @@
 
 /*
  * creating dynamic typed linked list
+ * LL_NODE_NAME is not literal const char*, it is struct's defined name
+ * LL is for Linked List
  */
 
-#define CREATE_LINKED_LIST_NODE_STRUCT(TYPE, NODE_NAME)                        \
+#define CREATE_LINKED_LIST_NODE_STRUCT(LL_TYPE, LL_NODE_NAME)                  \
   typedef struct {                                                             \
-    TYPE value;                                                                \
+    LL_TYPE value;                                                             \
     void *next;                                                                \
-  }##NODE_NAME;
+  }##LL_NODE_NAME;
 
-#define CREATE_LINKED_LIST_NODE(VARIABLE, VALUE, NODE_NAME)                    \
-  VARIABLE = malloc(sizeof(NODE_NAME));                                        \
-  memset(VARIABLE, 0, sizeof(NODE_NAME));                                      \
-  (VARIABLE)->value = VALUE;
+#define CREATE_LINKED_LIST_NODE(LL_VARIABLE, LL_VALUE, LL_NODE_NAME)           \
+  LL_VARIABLE = malloc(sizeof(LL_NODE_NAME));                                  \
+  memset(LL_VARIABLE, 0, sizeof(LL_NODE_NAME));                                \
+  (LL_VARIABLE)->value = LL_VALUE;
 
-#define ADD_LAST_LINKED_LIST(HEAD_NODE, END_NODE, VALUE, NODE_NAME)            \
+#define ADD_LAST_LINKED_LIST(LL_HEAD_NODE, LL_END_NODE, LL_VALUE,              \
+                             LL_NODE_NAME)                                     \
   {                                                                            \
-    NODE_NAME *temp_node = NULL;                                               \
-    CREATE_LINKED_LIST_NODE(temp_node, VALUE, NODE_NAME)                       \
-    if (NULL == (HEAD_NODE)) {                                                 \
-      (HEAD_NODE) = temp_node;                                                 \
-      (END_NODE) = HEAD_NODE;                                                  \
+    LL_NODE_NAME *temp_node = NULL;                                            \
+    CREATE_LINKED_LIST_NODE(temp_node, LL_VALUE, LL_NODE_NAME)                 \
+    if (NULL == (LL_HEAD_NODE)) {                                              \
+      (LL_HEAD_NODE) = temp_node;                                              \
+      (LL_END_NODE) = LL_HEAD_NODE;                                            \
     } else {                                                                   \
-      (END_NODE)->next = (NODE_NAME *)temp_node;                               \
-      (END_NODE) = temp_node;                                                  \
+      (LL_END_NODE)->next = (LL_NODE_NAME *)temp_node;                         \
+      (LL_END_NODE) = temp_node;                                               \
     }                                                                          \
   }
 
-#define ADD_BEGIN_LINKED_LIST(HEAD_NODE, END_NODE, VALUE, NODE_NAME)           \
+#define ADD_BEGIN_LINKED_LIST(LL_HEAD_NODE, LL_END_NODE, LL_VALUE,             \
+                              LL_NODE_NAME)                                    \
   {                                                                            \
-    NODE_NAME *temp_node = NULL;                                               \
-    CREATE_LINKED_LIST_NODE(temp_node, VALUE, NODE_NAME)                       \
-    if (NULL == (HEAD_NODE)) {                                                 \
-      (HEAD_NODE) = temp_node;                                                 \
-      (END_NODE) = HEAD_NODE;                                                  \
+    LL_NODE_NAME *temp_node = NULL;                                            \
+    CREATE_LINKED_LIST_NODE(temp_node, LL_VALUE, LL_NODE_NAME)                 \
+    if (NULL == (LL_HEAD_NODE)) {                                              \
+      (LL_HEAD_NODE) = temp_node;                                              \
+      (LL_END_NODE) = LL_HEAD_NODE;                                            \
     } else {                                                                   \
-      temp_node->next = HEAD_NODE;                                             \
-      (HEAD_NODE) = temp_node;                                                 \
+      temp_node->next = LL_HEAD_NODE;                                          \
+      (LL_HEAD_NODE) = temp_node;                                              \
     }                                                                          \
   }
 
-#define FREE_LINKED_LIST(HEAD_NODE, NODE_NAME)                                 \
+#define FREE_LINKED_LIST(LL_HEAD_NODE, LL_NODE_NAME)                           \
   {                                                                            \
-    while (NULL != (HEAD_NODE)) {                                              \
-      NODE_NAME *temp = HEAD_NODE;                                             \
-      (HEAD_NODE) = (HEAD_NODE)->next;                                         \
+    while (NULL != (LL_HEAD_NODE)) {                                           \
+      LL_NODE_NAME *temp = LL_HEAD_NODE;                                       \
+      (LL_HEAD_NODE) = (LL_HEAD_NODE)->next;                                   \
       free(temp);                                                              \
     }                                                                          \
-    (HEAD_NODE) = NULL;                                                        \
+    (LL_HEAD_NODE) = NULL;                                                     \
+  }
+
+#define REMOVE_HEAD_LINKED_LIST(LL_HEAD_NODE, LL_NODE_NAME)                    \
+  {                                                                            \
+    LL_NODE_NAME *holder = LL_HEAD_NODE;                                       \
+    (LL_HEAD_NODE) = (LL_HEAD_NODE)->next;                                     \
+    free(holder);                                                              \
+  }
+
+#define REMOVE_VALUE_LINKED_LIST(LL_HEAD_NODE, LL_VALUE, LL_NODE_NAME)         \
+  {                                                                            \
+    if ((LL_VALUE) == ((LL_HEAD_NODE)->value)) {                               \
+      REMOVE_HEAD_LINKED_LIST(LL_HEAD_NODE, LL_NODE_NAME)                      \
+    } else {                                                                   \
+      LL_NODE_NAME *iter = (LL_HEAD_NODE)->next;                               \
+      LL_NODE_NAME *iter_prev = (LL_HEAD_NODE);                                \
+      while (NULL != iter) {                                                   \
+        if (LL_VALUE == iter->value) {                                         \
+          iter_prev->next = iter->next;                                        \
+          free(iter);                                                          \
+          break;                                                               \
+        }                                                                      \
+        iter_prev = iter;                                                      \
+        iter = iter->next;                                                     \
+      }                                                                        \
+    }                                                                          \
   }
 
 #endif
--- main.c
+++ main.c
@@ -2,20 +2,22 @@
 
 #include "includes/linked_list.h"
 
-CREATE_LINKED_LIST_NODE_STRUCT(const char *, llnode_t)
+CREATE_LINKED_LIST_NODE_STRUCT(int, llnode_t)
 
 int main() {
   llnode_t *head_node = NULL;
   llnode_t *end_node = NULL;
 
-  ADD_LAST_LINKED_LIST(head_node, end_node, "asdasd", llnode_t)
-  ADD_LAST_LINKED_LIST(head_node, end_node, "asdzxcwewq", llnode_t)
-  ADD_BEGIN_LINKED_LIST(head_node, end_node, "zxcasdqwe", llnode_t)
+  ADD_LAST_LINKED_LIST(head_node, end_node, 10, llnode_t)
+  ADD_LAST_LINKED_LIST(head_node, end_node, 9, llnode_t)
+  ADD_BEGIN_LINKED_LIST(head_node, end_node, 8, llnode_t)
+
+  REMOVE_VALUE_LINKED_LIST(head_node, 8, llnode_t)
 
   llnode_t *iter = head_node;
 
   while (NULL != iter) {
-    printf("%s \n\r", iter->value);
+    printf("%d \n\r", iter->value);
     iter = iter->next;
   }
 
