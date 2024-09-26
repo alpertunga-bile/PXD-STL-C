0a397dcbe3a99b8dab3c1631e0a566b5b661c268
alpertunga-bile <kingdom19online@hotmail.com>
2024-09-26 12:00:13 +0300

    hash is added

---
+++ includes/hash.h
@@ -0,0 +1,8 @@
+#ifndef HASH_H
+#define HASH_H
+
+#include <stdint.h>
+
+size_t get_hash_number_value(float value);
+
+#endif
---
+++ sources/hash.c
@@ -0,0 +1,5 @@
+#include "hash.h"
+
+#include "blake3/c/blake3.h"
+
+size_t get_hash_number_value(float value) {}
